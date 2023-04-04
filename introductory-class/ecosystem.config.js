de691d3463bdf5f2a5e54e5f2f618368d9bf7c89
Rezuan-Ahmed-git <rezuan.ahmed.pro@gmail.com>
2023-04-04 16:12:56 +0600

    introductory-class

---
+++ introductory-class/ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    apps : [{
+      name   : "express-app",
+      script : "yarn",
+      args: "start",
+      cwd: "./express-app"
+    },
+    {
+        name   : "react-app",
+        script : "yarn",
+        args: "start",
+        cwd: "./react-app"
+      },
+],
+  }
